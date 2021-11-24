cd8b4dcc7fb8f06c5013b3f2f3cf8c299ea844b7
Jay9011 <whdtjq5904@gmail.com>
2021-11-24 11:18:40 +0900

    전처리기 (define)

--- Study_Cpp/Study_Cpp/maincpp.cpp
+++ Study_Cpp/Study_Cpp/maincpp.cpp
@@ -1,8 +1,19 @@
+/**
+*	▒ 전처리기 ▒
+*	컴파일 과정에서 컴파일러가 가장 먼저 처리하는 과정.
+* 
+*	#define	: 지정한 구문을 특정 숫자로 치환
+*		① 컴파일 과정에서 해당 구문을 해당 숫자로 바꿔주고 다음 컴파일을 진행한다.
+*		② 가독성이 좋아진다.
+*		③ 유지보수가 좋다.
+*/
+#define HUNGRY 1;
+
 extern int S01_DataType();
 extern int S02_Operator();
 extern int S03_Condition();
 
 int main()
 {
-	S03_Condition();
+	S02_Operator();
 }
