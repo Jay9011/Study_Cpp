eba26c1f6359414e8e53ac7e5a0e101b09120df5
Jay9011 <whdtjq5904@gmail.com>
2021-11-26 14:12:07 +0900

    재귀 함수

--- Study_Cpp/Study_Cpp/maincpp.cpp
+++ Study_Cpp/Study_Cpp/maincpp.cpp
@@ -2,8 +2,10 @@ extern int S01_DataType();
 extern int S02_Operator();
 extern int S03_Condition();
 extern int S04_BitOperator();
+extern int S05_Variable();
+extern int S06_RecursiveFunction();
 
 int main()
 {
-	S04_BitOperator();
+	S06_RecursiveFunction();
 }
