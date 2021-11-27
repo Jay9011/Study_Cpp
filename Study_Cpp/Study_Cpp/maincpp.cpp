d7c681d5b8ea9b816a55926dd1a43548bce007f7
Jay9011 <whdtjq5904@gmail.com>
2021-11-27 14:16:07 +0900

    분할 구현(header, #include), 외부 변수 (extern)

--- Study_Cpp/Study_Cpp/maincpp.cpp
+++ Study_Cpp/Study_Cpp/maincpp.cpp
@@ -1,4 +1,5 @@
-#include "S07_Implementation.h";
+#include "stdio.h"
+#include "S07_Implementation.h"
 
 extern int S01_DataType();
 extern int S02_Operator();
@@ -7,9 +8,14 @@ extern int S04_BitOperator();
 extern int S05_Variable();
 extern int S06_RecursiveFunction();
 
+// 외부 변수 선언부
+extern int _iExternV;
+
 int main()
 {
 	S06_RecursiveFunction();
 
 	int iNum1 = add(1, 2);
+
+	printf("_iExternV 값 : %d", _iExternV);
 }
