69426bcc5e7521ce3ab1942020614c788cb2351d
Jay9011 <whdtjq5904@gmail.com>
2022-03-14 18:05:35 +0900

    가변배열 클래스로 iterator 구현 (begin, end)

--- 2021_059_Iterator/main.cpp
+++ 2021_059_Iterator/main.cpp
@@ -2,29 +2,37 @@
 #include <vector>
 #include <list>
 
+using std::cout;
+using std::endl;
+using std::vector;
+using std::list;
+
 /*
 *	iterator
 */
 
 int main()
 {
-	std::vector<int> vecInt;
+	vector<int> vecInt;
 	vecInt.push_back(10);
 	vecInt.push_back(20);
-	std::cout << vecInt.at(0) << std::endl;		// 특정 위치의 노드를 가져온다.
+	cout << vecInt.at(0) << endl;		// 특정 위치의 노드를 가져온다.
 
-	std::list<int> listInt;
+	list<int> listInt;
 	listInt.push_back(10);
 	listInt.push_back(20);
 
-	std::vector<int>::iterator vecIter = vecInt.begin();	// vecIter가 vecInt의 첫 번째 요소를 가리키게 한다.
+	vector<int>::iterator vecIter = vecInt.begin();	// vecIter가 vecInt의 첫 번째 요소를 가리키게 한다.
 	*vecIter = 100;		// iterator가 가리키는 위치의 값을 수정.
 	vecInt[0] = 100;	// 위와 같은 동작
 
-	std::list<int>::iterator listIter = listInt.begin();
+	list<int>::iterator listIter = listInt.begin();
 	int iData = *listIter;
 	++listIter;			// 다음 노드를 가리키게 한다.
 	iData = *listIter;
+
+	// *myIterator : 현재 iterator가 가리키고 있는 데이터를 가리켜야 한다.
+	// ++ (전위, 후위), -- (전위, 후위)
 
 	return 0;
 }
--- 2021_059_Iterator/myArr.h
+++ 2021_059_Iterator/myArr.h
@@ -5,38 +5,47 @@
 template<typename T>
 class MyArr
 {
+private:
+	T*	m_pData;
+	int	m_iCount;
+	int	m_iMaxCount;
+
 public:
 	MyArr();
 	~MyArr();
-
-private:
-	T*		m_pData;
-	int		m_iCount;
-	int		m_iManxCount;
 
 public:
 	void push_back(const T& _Data);
 	void resize(int _iResizeCount);
 	T* data() { return this->m_pData };
 	int size() { return this->m_iCount };
-	int capacity() { return this->m_iManxCount };
+	int capacity() { return this->m_iMaxCount };
 	T& operator[] (int idx);
 
 	class iterator;		// 전방선언
 	iterator begin();
+	iterator end();
 
 public:
+	// inner 클래스를 포함하고 있는 클래스의 private 멤버에 접근 가능
 	class iterator
 	{
+	private:
+		MyArr*	m_pArr;		// iterator가 가리킬 데이터를 관리하는 가변배열 주소
+		T*		m_pData;	// 데이터들의 시작 주소
+		int		m_iIdx;		// 가리키는 데이터의 인덱스
+
 	public:
 		iterator()
-			: m_pData(nullptr)
+			: m_pArr(nullptr)
+			, m_pData(nullptr)
 			, m_iIdx(-1)
 		{
-
+			
 		}
-		iterator(T* _pData, int _iIdx)
-			: m_pData(_pData)
+		iterator(MyArr* _pArr, T* _pData, int _iIdx)
+			: m_pArr(_pArr)
+			, m_pData(_pData)
 			, m_iIdx(_iIdx)
 		{
 
@@ -46,21 +55,87 @@ public:
 
 		}
 
-	private:
-		T*		m_pData;
-		int		m_iIdx;
+	public:
+		T& operator * ()
+		{
+			// iterator 가 알고 있는 주소와, 가변배열이 알고 있는 주소가 달라진 경우 (공간 확장으로 주소가 달라진 경우)
+			// iterator 가 end iterator 인 경우
+			if (this->m_pArr->m_pData != this->m_pData || -1 == this->m_iIdx)
+				assert(nullptr);
+
+			return this->m_pData[this->m_iIdx];
+		}
+
+		iterator& operator ++ ()
+		{
+			return *this;
+		}
+
+		iterator& operator -- ()
+		{
+			return *this;
+		}
+
+		// end class iterator
 	};
+	// end class MyArr
 };
 
 template<typename T>
 inline MyArr<T>::MyArr()
 	: m_pData(nullptr)
+	, m_iCount(0)
+	, m_iMaxCount(2)
 {
+	this->m_pData = new T[2];
 }
 
 template<typename T>
 inline MyArr<T>::~MyArr()
 {
+	delete[] this->m_pData;
+}
+
+template<typename T>
+inline void MyArr<T>::push_back(const T& _Data)
+{
+	// 힙 영역에 할당한 공간이 다 참
+	if (this->m_iMaxCount <= this->m_iCount)
+	{
+		// 재할당
+		resize(m_iMaxCount * 2);
+	}
+
+	// 데이터 추가
+	this->m_pData[this->m_iCount++] = _Data;
+}
+
+template<typename T>
+inline void MyArr<T>::resize(int _iResizeCount)
+{
+	// 현재 최대 수용량보다 더 적은 수치로 확장하려는 경우
+	if (this->m_iMaxCount >= _iResizeCount)
+	{
+		assert(nullptr);
+	}
+
+	// 1. 리사이즈 시킬 개수만큼 동적할당 한다.
+	T* pNew = new T[_iResizeCount];
+
+	// 2. 기존 공간에 있던 데이터들을 새로 할당한 공간으로 복사시킨다.
+	for (int i = 0; i < this->m_iCount; ++i)
+	{
+		pNew[i] = this->m_pData[i];
+	}
+
+	// 3. 기존 공간은 메모리 해제
+	delete[] this->m_pData;
+
+	// 4. 배열이 새로 할당된 공간을 가리키게 한다.
+	this->m_pData = pNew;
+
+	// 5. Max Count 변경점 적용
+	this->m_iMaxCount = _iResizeCount;
 }
 
 template<typename T>
@@ -72,5 +147,19 @@ T& MyArr<T>::operator[](int idx)
 template<typename T>
 typename MyArr<T>::iterator MyArr<T>::begin()
 {
-	return iterator(m_pData, 0);
+	if (0 == this->m_pData)
+	{
+		return iterator(this, this->m_pData, -1);	// 데이터가 없는 경우, begin() == end()
+	}
+	else
+	{
+		return iterator(this, this->m_pData, 0);	// this 는 자기 자신을 가리키는 포인터
+	}
+}
+
+template<typename T>
+typename MyArr<T>::iterator MyArr<T>::end()
+{
+	// 끝을 가리키는 포인터는 인덱스를 -1로 생각한다.
+	return iterator(this, this->m_pData, -1);
 }
